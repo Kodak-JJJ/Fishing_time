d916a7c6de03606adb87eec9965160fc70d32630
Koki Sugisaka <128578440+Kodak-JJJ@users.noreply.github.com>
2025-11-22 23:33:03 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,54 +1,24 @@
-var express = require('express');
-const bodyParser = require('body-parser');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const axios = require('axios');
-const cheerio = require('cheerio');
-const { response } = require('express');
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
-var homepageRouter = require('./routes/home_page');
-var tidalLocationRouter = require('./routes/tidal_location');
-var freshLocationRouter = require('./routes/fresh_location');
-
-var app = express();
-
-
-
-
-
-app.listen(PORT, () => {
-  console.log("Server running on http://localhost:3000");
-});
-app.set('view engine', 'ejs')
-.listen(PORT,() => console.log(`server is working on ${PORT}`));
+app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
 
 app.use(logger('dev'));
 app.use(express.json());
-//ask chatgpt about this later
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-//ask chatgpt about this later
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/home_page',homepageRouter);
-app.use('/tidal_location',tidalLocationRouter);
-app.use('/river_location',freshLocationRouter);
-// catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//     next(createError(404));
-//   });
+app.use('/home_page', homepageRouter);
+app.use('/tidal_location', tidalLocationRouter);
+app.use('/river_location', freshLocationRouter);
 
 // error handler
 app.use(function(err, req, res, next) {
-  console.error(err); // keep the stack in server logs
+  console.error(err);
 
   if (res.headersSent) return next(err);
 
-  // Send plain HTML or JSON instead of rendering a view
   if (req.accepts('html')) {
     res.status(err.status || 500).send(
       `<h1>Server Error</h1><pre>${err.message}</pre>`
@@ -56,6 +26,11 @@ app.use(function(err, req, res, next) {
   } else {
     res.status(err.status || 500).json({ error: err.message });
   }
-  });
+});
+
+// 👉 Only ONE listen, at the very end
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 module.exports = app;
