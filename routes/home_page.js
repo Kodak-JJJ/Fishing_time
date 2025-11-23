0c33e5100e476e4726c3181573a368fc0d19b870
Koki Sugisaka <128578440+Kodak-JJJ@users.noreply.github.com>
2025-11-23 01:26:52 -0800

    Update home_page.js

--- routes/home_page.js
+++ routes/home_page.js
@@ -1,30 +1,33 @@
-var express = require('express');
-
-var router = express.Router();
+const express = require('express');
 const bodyParser = require('body-parser');
-var connection = require('../database');
-var urlencodedParser = bodyParser.urlencoded({extended: false});
+const pool = require('../database');   // ✅ use the pool, not old connection
 
-router.get('/',(req, res) => {
-    var tidal_sql= "SELECT * FROM tidal_locations";
-    var river_sql= "SELECT * FROM river_locations";
+const router = express.Router();
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-    connection.query(tidal_sql,function(err, tidal_location_data){
-        if(err) throw err;
-        connection.query(river_sql,function(err, river_location_data){
-            if(err) throw err;
+router.get('/', async (req, res, next) => {
+  try {
+    const tidal_sql = "SELECT * FROM tidal_locations";
+    const river_sql = "SELECT * FROM river_locations";
 
-            console.log(river_location_data);
-            console.log(tidal_location_data);
-            res.render('_home_page',
-            {
-                tidal_location_data: tidal_location_data,
-                river_location_data: river_location_data
-            });
-        });
+    // You can run them sequentially (simple & fine):
+    const [tidal_location_data] = await pool.query(tidal_sql);
+    const [river_location_data] = await pool.query(river_sql);
 
+    console.log(river_location_data);
+    console.log(tidal_location_data);
+
+    res.render('_home_page', {
+      tidal_location_data,
+      river_location_data
     });
+  } catch (err) {
+    // Don’t throw; pass to Express error handler
+    next(err);
+  }
 });
+
+module.exports = router;
 
 
 module.exports = router;
